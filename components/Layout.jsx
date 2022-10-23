ff95cc5dbc446d6b87f3cabf31382d4ae6f3a626
veektrie <102025110+veektrie@users.noreply.github.com>
2022-10-23 18:51:04 +0100

    footer banner created

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -1,7 +1,21 @@
 import React from "react";
+import Head from "next/head";
+import Navbar from "./Navbar";
+import Footer from "./Footer";
 
 const Layout = () => {
-  return <div>Layout</div>;
+  return (
+    <div className="layout">
+      <Head>Victory Accesories Store</Head>
+      <header>
+        <Navbar />
+      </header>
+      <main className="main-container">EMPTY</main>
+      <footer>
+        <Footer />
+      </footer>
+    </div>
+  );
 };
 
 export default Layout;
