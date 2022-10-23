9df319ef210d013963d2b3b8a74f15e0cdde7757
veektrie <102025110+veektrie@users.noreply.github.com>
2022-10-23 08:42:34 +0100

    prodcuts component added

--- pages/index.js
+++ pages/index.js
@@ -12,7 +12,7 @@ const Home = ({products,bannerData}) => {
         <p>Speakers of many variations</p>
       </div>
       <div className='products-container'>
-        {products?.map((product) => product.name)}
+        {products?.map((product) => <Product key={product._id} product={product } />)}
       </div>
 
       <FooterBanner/>
