e8cdb77ec488dbba9d1a9db5ee6662321bfb98a8
veektrie <102025110+veektrie@users.noreply.github.com>
2022-10-26 03:02:38 +0100

    product detail slug updated

--- pages/product/[slug].js
+++ pages/product/[slug].js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { AiFillStar, AiOutlineMinus, AiOutlinePlus, AiOutlineStar } from 'react-icons/ai';
 import { client, urlFor } from '../../lib/client';
 
 const ProductDetails = ({ product, products }) => {
@@ -9,6 +10,56 @@ const ProductDetails = ({ product, products }) => {
         <div>
           <div className='image-container'>
             <img src={urlFor(image && image[0])}/>
+          </div>
+          {/* <div className='small-images-container'>
+            {image?.map((item, i) => (
+              <img
+                src={urlFor(item)}
+                className=''
+                onMouseEnter=''
+              />
+            ))}
+          </div> */}
+        </div>
+        <div className='product-detail-desc'>
+          <h1>{name}</h1>
+          <div className='reviews'>
+            <div>
+              <AiFillStar/>
+              <AiFillStar/>
+              <AiFillStar/>
+              <AiFillStar/>
+              <AiOutlineStar/>
+            </div>
+            <p>(20)</p>
+
+          </div>
+          <h4>Details:</h4>
+          <p>{details}</p>
+          <p className='price'>${price}</p>
+          <div className='quantity'>
+            <h3>Quantity:</h3>
+            <p className='quantity-desc'>
+              <span className='minus'
+                onClick=''>
+                <AiOutlineMinus/>
+
+              </span>
+              <span className='num'
+                onClick=''>
+                0
+              </span>
+              <span className='plus'
+                onClick=''>
+                <AiOutlinePlus/>
+
+              </span>
+
+            </p>
+          </div>
+          <div className='buttons'>
+            <button type='button' className='add-to-cart' onClick=''>Add to Cart</button>
+            <button type='button' className='buy-now' onClick=''>Buy Now</button>
           </div>
         </div>
       </div>
