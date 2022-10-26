8d42847f8260c92956ace6c7cd6ec62bd8f82691
veektrie <102025110+veektrie@users.noreply.github.com>
2022-10-26 08:20:40 +0100

    cart component

--- components/Cart.jsx
+++ components/Cart.jsx
@@ -1,6 +1,20 @@
-import React from "react";
+import React, { useRef } from "react";
+import Link from "next/link";
+import {
+  AiOutlineMinus,
+  AiOutlineLeft,
+  AiOutlinePlus,
+  AiOutlineShopping,
+} from "react-icons/ai";
+import { TiDeleteOutline } from "react-icons/ti";
+import toast from "react-hot-toast";
+import { useStateContext } from "../context/StateContext";
+import { urlFor } from "../lib/client";
 
 const Cart = () => {
+  const cartRef = useRef();
+  const { totalPrice, totalQuantities, cartitems, setShowCart } =
+    useStateContext();
   return <div>Cart</div>;
 };
 
