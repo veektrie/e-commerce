dc88c29e598bf7f63d5c82290687ee51989933e7
veektrie <102025110+veektrie@users.noreply.github.com>
2022-10-26 04:48:07 +0100

    hooks  created and implemented

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,19 @@
 import React from 'react';
 import { Layout } from '../components';
-import '../styles/globals.css'
+import { StateContext } from '../context/StateContext';
+import '../styles/globals.css';
+import { Toaster } from 'react-hot-toast';
 
 function MyApp({ Component, pageProps }) {
-  return <Layout>
-    <Component {...pageProps} />
-    </Layout>
+  return (
+    <StateContext> 
+      <Layout >
+        <Toaster/>
+        <Component {...pageProps} /> 
+     </Layout>
+
+    </StateContext>
+  )
 }
 
 export default MyApp
